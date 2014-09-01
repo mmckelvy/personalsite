781d147f9d2f3263b5f5f9737b04948e68fd160a
Mark McKelvy <mmckelvy@gmail.com>
2014-08-31 23:37:16 -0600

    starting about section

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,18 @@
 		* UI Style functions.
 		**/
 
+		/* UI changes for nav links. */
 		$('.nav-list').on('click', 'a', function (e) {
-			$('.nav-item-link').removeClass('nav-item-active')
-			$(this).addClass('nav-item-active')
+			
+			// Change color of nav links.
+			$('.nav-item-link').removeClass('nav-item-active');
+			$(this).addClass('nav-item-active');
+			
+			// Animate the border for the main content.
+			$('.content-block').addClass('animated');
+
 		});
+
 
 
 	}); // End jQuery document ready.
